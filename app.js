9f82a25263f9f8d112a55f8478aa8cd15298ab1f
Jeff Hostetler <hostetjm@gmail.com>
2015-01-16 15:19:26 -0700

    set up mustache template, some jquery, songs not playing and track name in Now Playing not working

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-"use strict";
-$('.container').on('click', '.fa-play', function(){
-  $('i').removeClass('fa-stop');
-  $('i').addClass('fa-play');
-  $(this).removeClass('fa-play');
-  $(this).addClass('fa-stop');
-})
-$('.container').on('click', '.fa-stop', function(){
-  $(this).removeClass('fa-stop');
-  $(this).addClass('fa-play');
-})
+$.getJSON('data.json', function(tracks){
+  var $tracksTemplate = $('.tracksTemplate').html();
+  var newHTML = Mustache.to_html($tracksTemplate, tracks);
 
+  $('.tracks').html(newHTML);
 
+  $('.container').on('click', '.fa-play', function(){
+    $('i').removeClass('fa-stop');
+    $('i').addClass('fa-play');
+    $(this).removeClass('fa-play');
+    $(this).addClass('fa-stop');
+    $('h1').text("Now playing: "  );
+    document.getElementById("thrill-of-the-hunt").play();
 
-$('div').on('mouseenter', function(){
-  document.getElementById('breathing').play();
-  setTimeout(
-    function()
-  {
-    $('body').addClass('scare')
-    document.getElementById('scream').play();
-  }, 15000);
-})
-$('div').on('mouseleave', function(){
-  document.getElementById('honk').play();
-})
+  })
+  $('.container').on('click', '.fa-stop', function(){
+    $(this).removeClass('fa-stop');
+    $(this).addClass('fa-play');
+    $('h1').text( "Select a Song!" );
+  })
+});
